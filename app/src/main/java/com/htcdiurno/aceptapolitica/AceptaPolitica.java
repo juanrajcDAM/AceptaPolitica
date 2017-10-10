51a02ac97133762c467bf574cddb8eb4c3bd22fc
juanrajc <juanrajc@hotmail.com>
2017-10-10 19:48:44 +0200

    No aparece el mensaje devuelto al main.

---
+++ app/src/main/java/com/htcdiurno/aceptapolitica/AceptaPolitica.java
@@ -0,0 +1,61 @@
+package com.htcdiurno.aceptapolitica;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AceptaPolitica extends AppCompatActivity {
+
+    private final static int CODIGO = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btAct = (Button)findViewById(R.id. button);
+
+        btAct.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirVerifica();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CODIGO && resultCode == RESULT_OK) {
+
+            TextView txtMensaje = (TextView)findViewById(R.id.textView);
+            String res = data.getExtras().getString("OPCION");
+            txtMensaje.setText("Resultado: " + res);
+
+        }
+
+    }
+
+    public void abrirVerifica(){
+
+        EditText etNombre = (EditText)findViewById(R.id. editText);
+        String nombre = etNombre.getText().toString();
+
+        EditText etEdad = (EditText)findViewById(R.id. editText2);
+        String edad = etEdad.getText().toString();
+
+        Intent intent= new Intent (this, Verifica. class);
+        Bundle bundle = new Bundle();
+        bundle.putString( "NOMBRE", nombre);
+        bundle.putString( "EDAD", edad);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/htcdiurno/aceptapolitica/Verifica.java
@@ -0,0 +1,67 @@
+package com.htcdiurno.aceptapolitica;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Verifica extends AppCompatActivity {
+
+    private final static int CODIGO = 0;
+
+    private boolean flag;
+    private String edad;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifica);
+
+        TextView txtMensaje =(TextView)findViewById(R.id.texto);
+        Bundle bundle = getIntent().getExtras();
+        txtMensaje.setText( "Hola "+ bundle.getString("NOMBRE")+
+                " ¿Aceptas la política de privacidad?");
+
+        edad=bundle.getString("EDAD");
+
+        Button btAct2 = (Button)findViewById(R.id. button2);
+        btAct2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag=true;
+                volverMain(edad);
+            }
+        });
+
+        Button btAct3 = (Button)findViewById(R.id. button3);
+        btAct3.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag=false;
+                volverMain(edad);
+            }
+        });
+    }
+
+    public void volverMain(String edad){
+
+        Intent intent= new Intent (this, AceptaPolitica. class);
+        Bundle bundle = new Bundle();
+
+        if(flag) {
+
+            if (Integer.parseInt(edad) < 18)
+                bundle.putString("OPCION", "Aceptada con restricciones.");
+            else
+                bundle.putString("OPCION", "Aceptada sin restricciones.");
+
+        }else
+            bundle.putString( "OPCION", "Rechazada");
+
+        intent.putExtras(bundle);
+        startActivityForResult(intent, CODIGO);
+
+    }
+
+}
